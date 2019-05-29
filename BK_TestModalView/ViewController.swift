576f3979793ea001dde4eaf8463bccca3c437585
Bene <bk191414@gmail.com>
2019-05-29 09:07:00 +0200

    Initial Commit

---
+++ BK_TestModalView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BK_TestModalView
+//
+//  Created by Benedikt Kurz on 29.05.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
