946a9ec7f8485482e2232a0a9b9b9b8bab5c1958
Bene <bk191414@gmail.com>
2019-06-05 09:20:36 +0200

    Callbacks wurden implementiert

--- BK_TestModalView/SecondViewController.swift
+++ BK_TestModalView/SecondViewController.swift
@@ -13,53 +13,34 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var lblDate: UILabel!
     var observer: NSObjectProtocol?
      
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //    Notificat ionCenter.default.addObserver(self, selector: #selector(handlePopupClosing), name: .saveDateTime, object: nil)
-       
-        
-    }
-    
-    
-    /*
-    override func viewDidAppear(_ animated: Bool) {
-        
-        observer = NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main, using: { (notification) in
-            let dateVC = notification.object as! DatePopupViewController
-            self.lblDate.text = dateVC.formattedDate
-        })
-        
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        
-        if let observer = observer {
-            
-            NotificationCenter.default.removeObserver(observer)
-        }
-        
-    }
-    */
 
     
-    
-    
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         
-        if segue.identifier == "toDatePopupViewControllerSegue" {
+        // Code
+        if segue.identifier == "toDatePopupViewControllerSeuge" {
             
             let popup = segue.destination as! DatePopupViewController
+            
             popup.showTimePicker = false
             
-            
+            // 1. Assign to a function
+            // popup.onSave = onSave
+            // 2. Use a Closure
+            popup.onSave = { (data) in
+                self.lblDate.text = data
+                
+            }
         }
+    }
+    
+    func onSave(_ data: String) -> () {
+        
+        lblDate.text = data
+        
     }
     
 
